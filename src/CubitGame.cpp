0d750ba6a0f2d0fbebac12b3a0d4393457183b39
nrp3c <pennington.noah@gmail.com>
2016-11-12 13:12:41 -0600

    we got background now boys

--- src/CubitGame.cpp
+++ src/CubitGame.cpp
@@ -1,22 +1,33 @@
 #include "CubitGame.h"
+#include <SFML/OpenGL.hpp>
 
 void CubitGame::setupDisplay()
 {
-        window.create(sf::VideoMode(800,600), "My Window");
+        window.create(sf::VideoMode(800,600), "OpenGL", sf::Style::Default);
         window.setFramerateLimit(60);
 }
 
 void CubitGame::runLoop()
 {
+//      Player player1;
         while (window.isOpen())
         {
                 // check all the window's events that were triggered since the last iteration of the loop
                 while (window.pollEvent(event))
                 {
+// generate level
                         // "close requested" event: we close the window
                         if (event.type == sf::Event::Closed)
                                 window.close();
+
+//                        if(player.keyboard = 'w')
+//                        player.move();
+//                        spawns enemies ()
+//                        end if dead()
+
                 }
+                window.display();
+
         }
 }
 
