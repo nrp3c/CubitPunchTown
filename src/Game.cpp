6cd2b3a715191f60428915c742501654b2e29f66
nrp3c <pennington.noah@gmail.com>
2016-11-12 14:40:36 -0600

    Added placeholder input

--- src/Game.cpp
+++ src/Game.cpp
@@ -11,8 +11,11 @@ void CubitGame::setupDisplay()
 
 void CubitGame::runLoop()
 {
+        Player player1;
+
         while(window.pollEvent(event))
         {
+                sf::Vector2i MousePos = sf::Mouse::getPosition();
                 switch(event.type)
                 {
                 case sf::Event::Closed:
@@ -21,6 +24,17 @@ void CubitGame::runLoop()
                 default:
                         break;
                 }
+
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+                        player1.moveUp();
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+                        player1.moveDown();
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+                        player1.moveLeft();
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+                        player1.moveRight();
+                if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+                        player1.punch();
 
         }
 }
