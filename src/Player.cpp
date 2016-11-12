037b4384d2d500447d0ede8b5bf2b2fb7bbf6350
william <bluecactus98@gmail.com>
2016-11-12 14:05:07 -0600

    hwy

--- src/Player.cpp
+++ src/Player.cpp
@@ -5,3 +5,17 @@ using namespace sf;
 	Player::player() {
 		
 	}
+	void punch() {
+		
+	}
+	void lookAtMouse(RenderWindow &window) {
+		Vector2f curPos = square.getPosition();
+		Vextor2i position = Mouse::getPosition(window);
+		
+		float dx curPos.x - position.x;
+		float dy curPos.y - position.y;
+		
+		float rotation = (atan2(dy, dx)) * 180 / PI;
+		
+		square.setRotation(rotation+180);
+	}
--- src/Player.h
+++ src/Player.h
@@ -1,16 +1,17 @@
-#include "Entity"
+#include "Entity.h"
 
 #ifndef Player_h
 #define Player_h
 
 using namespace sf;
 
-class Player public Entitiy {
+class Player : public Entity {
 	private:
-		
+		float const PI = 3.14159
 	
 	public:
 		player();
-		void look();
+		void punch();
+		void lookAtMouse(RenderWindow);
 };
 #endif
