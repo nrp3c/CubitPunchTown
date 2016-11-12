be52b53b385eca6c87cc049c43b746636470c988
theAttractiveOne <bluecactus98@gmail.com>
2016-11-12 10:59:29 -0600

    Update Entity.cpp

---
+++ src/Entity.cpp
@@ -0,0 +1,30 @@
+#include "Entitiy.h"
+
+using namespace sf;
+
+Entity :: Entity (float x, float y, float s) {
+	position.x = x;
+	position.y = y;
+	speed = s;
+}
+
+void Entity::moveDown() {
+	position.y -= speed;
+}
+
+
+void Entity::moveUp() {
+	position.y += speed;
+}
+
+void Entity::moveLeft() {
+    position.x -= speed;
+}
+ 
+void Entity::moveRight() {
+    position.x += speed;
+}
+ 
+void Entity::update() {
+    square.setPosition(position);
+}
