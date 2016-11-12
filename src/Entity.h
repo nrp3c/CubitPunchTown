35cc393bd45ae90bb156b3491892e8afdf3d38b1
theAttractiveOne <bluecactus98@gmail.com>
2016-11-12 10:57:32 -0600

    Update Entity.h

---
+++ src/Entity.h
@@ -0,0 +1,23 @@
+#include <SFML/Graphics.hpp>
+
+#iifndef Entity_h
+#define Entity_h
+
+using namespace sf;
+
+class Entity.h {
+	private:
+		Vector2f position;
+		Entity(float, float, float) // X, Y, Speed
+		int health;
+		float speed;
+		RectangleShape square (const Vector2f &size=Vector2f(1, 1));
+	
+	public:
+		void moveUp();
+		void moveDown();
+		void moveLeft();
+		void moveRight();
+		void updat();
+};
+#endif
