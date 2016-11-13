6e086f8ebd1e7e36fde4f5e9ada7f0408bf0e536
william <bluecactus98@gmail.com>
2016-11-12 19:27:41 -0600

    whatever

--- src/Entity.h
+++ src/Entity.h
@@ -1,16 +1,12 @@
 #include <SFML/Graphics.hpp>
-<<<<<<< HEAD
-=======
 #include <string>
 #include <iostream>
->>>>>>> a587c5e35f78f0a2bafefe57176a12087dc72d0b
 
 #ifndef Entity_h
 #define Entity_h
 
 class Entity {
 	public:
-<<<<<<< HEAD
 	Entity(float, float, float); // X, Y, Speed
         Entity();
         void HorizontalMovement(char LorR);
@@ -18,7 +14,6 @@ class Entity {
 	void Decel();
 	void Move();
 	void rotate(float rot);
-=======
 		Entity(float, float, float, std::string); // X, Y, Speed, string filename
         Entity();
         void HorizontalMovement(char LorR);
@@ -35,7 +30,6 @@ class Entity {
 		int health;
 		float speed;
 //		RectangleShape square (const Vector2f &size=Vector2f(1, 1));
->>>>>>> a587c5e35f78f0a2bafefe57176a12087dc72d0b
 
 	
 
